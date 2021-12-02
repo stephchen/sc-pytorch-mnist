8d7bf812bf1f12dcf44344732738d1f581edaad9
stephchen <stephchen@wandb.com>
2021-12-02 10:59:28 -0500

    rm extra prints

--- launch_test.py
+++ launch_test.py
@@ -3,13 +3,9 @@ import os
 import time
 
 
-print('@@@@@@@@@@@@@@@@@@@@@@@')
-
 wandb.init()
 
-print('@@@@@@@@@ init')
 print('launch test!')
-
 print('sleeping.......')
 time.sleep(5)
 print('awake!')
